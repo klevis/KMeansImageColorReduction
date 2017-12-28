0be7d274368d13f215f42564e7caf3a09bee28ae
klevis.ramo <klevisgramo@gmail.com>
2017-12-29 00:13:13 +0100

    embed HADOOP_HOME inside project

--- src/main/java/ramo/klevis/ml/kmeans/Run.java
+++ src/main/java/ramo/klevis/ml/kmeans/Run.java
@@ -1,13 +1,33 @@
 package ramo.klevis.ml.kmeans;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by klevis.ramo on 9/18/2017.
  */
 public class Run {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
+        setHadoopHomeEnvironmentVariable();
         new KMeansUI();
+    }
+
+    private static void setHadoopHomeEnvironmentVariable() throws Exception {
+        HashMap<String, String> hadoopEnvSetUp = new HashMap<>();
+        hadoopEnvSetUp.put("HADOOP_HOME", new File("winutils-master/hadoop-2.8.1").getAbsolutePath());
+        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+        theEnvironmentField.setAccessible(true);
+        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+        env.clear();
+        env.putAll(hadoopEnvSetUp);
+        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+        theCaseInsensitiveEnvironmentField.setAccessible(true);
+        Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
+        cienv.clear();
+        cienv.putAll(hadoopEnvSetUp);
     }
 }
